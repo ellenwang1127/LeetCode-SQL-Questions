18a9794ea318d6896a03a74de35c118d760eaf77
ellenwang1127 <105130816+ellenwang1127@users.noreply.github.com>
2022-08-12 16:17:25 -0400

    Add files via upload

--- 1241. Number of Comments per Post.sql
+++ 1241. Number of Comments per Post.sql
@@ -21,5 +21,5 @@ insert into submissions values
 SELECT DISTINCT s1.sub_id post_id, COUNT(DISTINCT s2.sub_id) number_of_comments
 FROM submissions s1
 LEFT JOIN submissions s2 ON s1.sub_id=s2.parent_id
-WHERE s1.sub_id IS NULL
+WHERE s1.parent_id IS NULL
 GROUP BY 1
