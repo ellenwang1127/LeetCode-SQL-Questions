5771b796f4d1bfff7a5d6061430a34b21674644b
ellenwang1127 <105130816+ellenwang1127@users.noreply.github.com>
2022-10-25 00:35:42 -0400

    Add files via upload

--- 571. Find Median Given Frequency of Numbers.sql
+++ 571. Find Median Given Frequency of Numbers.sql
@@ -7,16 +7,30 @@ insert into numbers values
 (0,7),(1,1),(2,3),(3,1);
 
 
-#Solution:
+#Solution 1:
+WITH RECURSIVE rec_cte AS (
+SELECT number, frequency, 1 cnt FROM numbers
+UNION
+SELECT number, frequency, cnt+1 FROM rec_cte
+WHERE cnt<frequency
+),
+	t1 AS (
+SELECT *, ROW_NUMBER() OVER(ORDER BY number) row_num,
+		COUNT(number) OVER() count
+FROM rec_cte
+ORDER BY number, frequency, count    
+)
+
+SELECT AVG(number)
+FROM t1
+WHERE row_num BETWEEN count/2 AND count/2 + 1
+
+
+
+#Soltution 2:
 SELECT ROUND(AVG(number),2) median
 FROM
 (SELECT *, SUM(frequency) OVER (ORDER BY number) row_num,
 		  SUM(frequency) OVER () total_num
 FROM numbers) t1
 WHERE total_num/2 BETWEEN row_num - frequency AND row_num
-
-/*
-12:6,7 (total/2, total/2 +1)
-13:7   ((total+1)/2)
-14:7,8
-*/
